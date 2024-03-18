cfc6c00e6b20a6193d753eb2ee0743b5bf53af49
jj-sallz89 <gitboi56@outlook.com>
2024-03-18 22:31:31 +0000

    changes

--- src/components/Hightlights.jsx
+++ src/components/Hightlights.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
+
 
 const Hightlights = () => {
+  useGSAP(()=>{
+    gsap.to('#title', {opacity: 1, y:0 })
+  },[])
   return (
-    <section>
-      
+    <section id='highlights' className='w-screen overflow-hidden h-full common-padding bg-zinc'>
+<div className="screen-max-width">
+  <div className='mb-12 w-full items-end justify-between'>
+    <h1 id='title' className='section-heading'>Get the highlights</h1>
+    <div className="flex flex-wrap gap-5 items-end">
+      <p className="link">
+        watch the film
+        <img src  />
+      </p>
+
+    </div>
+  </div>
+</div>
     </section>
   )
 }
