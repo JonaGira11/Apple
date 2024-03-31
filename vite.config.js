dac489270a3e0d2d9ad2af8b9c1652e9063aba7d
jj-sallz89 <gitboi56@outlook.com>
2024-03-31 23:07:34 +0100

    added docker

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, preview } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    preview: {
+      port: 3000,
+      strictPort: true,
+    },
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+    origin: "https://localhost:3000",
+  },
+
+  });
